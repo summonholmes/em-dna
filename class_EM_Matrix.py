78adff43f14b715c3a85c67829726d2b98c51f04
Shane Kimble <shanekimble@Shanes-MacBook-Pro.local>
2018-07-03 17:18:55 -0400

    Rounding removed

--- class_EM_Matrix.py
+++ class_EM_Matrix.py
@@ -1,6 +1,6 @@
 from class_EM_Count import EM_Count
 from math import log
-from numpy import empty, round, vectorize
+from numpy import empty, vectorize
 
 
 class EM_Matrix(EM_Count):
@@ -34,26 +34,23 @@ class EM_Matrix(EM_Count):
             "motif_posits_g"]  # By Row
 
     def freq_matrix_convert_bgs(self):
-        self.em_log_odds_matrix[:, 0] = round(  # By Column
-            (self.em_log_odds_matrix[:, 0] / sum(
-                self.em_log_odds_matrix[:, 0])),
-            decimals=3)
+        self.em_log_odds_matrix[:, 0] = self.em_log_odds_matrix[:, 0] / sum(
+            self.em_log_odds_matrix[:, 0])
 
     def freq_matrix_convert_cols(self):
         col_totals = self.em_log_odds_matrix.sum(axis=0)
         for i in range(1, self.motif_width + 1):
-            self.em_log_odds_matrix[:, i] = round(  # By Column
-                (self.em_log_odds_matrix[:, i] / col_totals[i]),
-                decimals=3)
+            self.em_log_odds_matrix[:,
+                                    i] = self.em_log_odds_matrix[:,
+                                                                 i] / col_totals[i]
 
     def odds_matrix_populate(self):
         for i in range(4):
-            self.em_log_odds_matrix[i, 1:] = round(  # By Row
-                (self.em_log_odds_matrix[i, 1:] /
-                 self.em_log_odds_matrix[i][0]),
-                decimals=3)  # Odds of getting each base in each position
+            self.em_log_odds_matrix[i, 1:] = (  # By Row
+                self.em_log_odds_matrix[i, 1:] / self.em_log_odds_matrix[i][0]
+            )  # Odds of getting each base in each position
 
     def odds_matrix_to_log(self):
-        logger = vectorize(lambda x: round(log(x, 2), decimals=3))
+        logger = vectorize(lambda x: log(x, 2))
         self.em_log_odds_matrix = logger(
             self.em_log_odds_matrix[:, 1:])  # log-odds matrix is NOT offset!
